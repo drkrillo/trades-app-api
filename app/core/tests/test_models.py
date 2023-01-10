ed82a9db59f191cba0875bab45ee380e17522feb
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-01-10 17:29:06 -0300

    Fix Linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,7 +20,7 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-    
+
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
@@ -35,7 +35,7 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
         """Test creating a superuser"""
