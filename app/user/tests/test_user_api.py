b6db8e5ea70e8fdbd3261ef287e8c04de47850f1
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-01-10 17:24:42 -0300

    added Views and URLs for User API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -49,3 +49,17 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_password_too_short_error(self):
+        """Test an error is returned if password less than 5 characters."""
+        payload = {
+            'email': 'test@example.com',
+            'password': '123',
+            'name': 'testname',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+            ).exists()
+        self.assertFalse(user_exists)
