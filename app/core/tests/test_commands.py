273ad14c6db770040be261c9304c5e75fe5e58ff
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-01-20 21:46:12 -0300

    added test to populate crypto tables

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,18 +2,25 @@
 Test Django Custom management commands
 """
 
+from unittest import TestCase
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCase
+from django.test import (
+    TestCase,
+    SimpleTestCase,
+)
+from core.models import Crypto
+
+from rest_framework import status
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTests(SimpleTestCase):
-    """ Test Commands """
+class CommandDBTests(SimpleTestCase):
+    """ Test Database Commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready"""
@@ -34,3 +41,17 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
+
+
+@patch('core.management.commands.populate_crypto_tables.Command.check')
+class CommandCryptoTests(TestCase):
+    """Test Populate Crypto table command."""
+
+    def test_command_run_successfully(self, patched_check):
+        """Test command run successfully"""
+
+        patched_check.return_value = True
+
+        call_command('populate_crypto_tables')
+
+        self.assertEqual(Crypto.objects.all().count(), 900)
